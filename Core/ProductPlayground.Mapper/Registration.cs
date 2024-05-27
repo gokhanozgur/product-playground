f3d3095275da2fa3cb0915fb0ecf0937e41f3299
Gökhan ÖZGÜR <g.ozgur@outlook.com>
2024-05-27 20:33:47 +0300

    Add missing mapper class library

---
+++ Core/ProductPlayground.Mapper/Registration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductPlayground.Application.Interfaces.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPlayground.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
